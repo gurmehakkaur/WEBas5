8cfcc9f7826c02b2889031e3c027288b61cb2986
Gurmehak Kaur Uppal <156244537+gurmehakkaur@users.noreply.github.com>
2025-03-21 19:37:43 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,8 @@
 *  https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
 * 
 *  Name: ___Gurmehak Kaur Uppal_________________ Student ID: __145885232________________ Date: ____21-03-2025________________
-*Publish : 
-* GitHub: 
+*Publish : https://67ddf7c1c5172a350e8ebe3b--webas5.netlify.app/
+* GitHub:   https://github.com/gurmehakkaur/WEBas5/edit/main/pages/index.js
 ********************************************************************************/
 
 import { Container, Row, Col, Image } from 'react-bootstrap';
